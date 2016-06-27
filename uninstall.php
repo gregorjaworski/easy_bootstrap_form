08cfdb1e965ff22c28d0ab82785a3268f387a6e2
Grzegorz Jaworski <webmaster@fenixmedia.pl>
2016-06-27 13:09:23 +0200

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,10 +1,2 @@
 <?php
-register_activation_hook( __FILE__, 'boj_myplugin_activate' );
-function boj_myplugin_activate() {
-// Rejestracja funkcji odinstalowania.
-register_uninstall_hook( __FILE__, 'boj_myplugin_uninstaller' );
-}
-function boj_myplugin_uninstaller() {
-// Usunięcie wszelkich opcji, tabel itd. utworzonych przez wtyczkę.
-delete_option( 'boj_myplugin_options' );
-}
+
